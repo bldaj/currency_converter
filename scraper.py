89027917a416e9b9ffaa2bef78fb597309844d0b
kk <kkaloshin@dengabank.ru>
2021-01-31 23:43:30 +0300

    refactor: Split code to functions

--- scraper.py
+++ scraper.py
@@ -3,20 +3,30 @@ from urllib.request import urlopen
 
 from settings import CBR_URL
 
-page = urlopen(CBR_URL)
-html = page.read().decode('utf-8')
 
-data = re.findall(r'<td>(.*)</td>', html)
+def get_data_from_site() -> list:
+    """"""
+    page = urlopen(CBR_URL)
+    html = page.read().decode('utf-8')
 
-offset = 0
-total_count = len(data)
-iterations = total_count // 5
+    return re.findall(r'<td>(.*)</td>', html)
 
-result = dict()
-for i in range(iterations):
-    current_currency = data[offset:offset + 5]
-    result[current_currency[1]] = {
-        'quantity': int(current_currency[2]),
-        'rate': float(current_currency[4].replace(',', '.'))
-    }
-    offset += 5
+
+def get_currency() -> dict:
+    """"""
+    data = get_data_from_site()
+
+    offset = 0
+    total_count = len(data)
+    iterations = total_count // 5
+
+    result = dict()
+    for i in range(iterations):
+        current_currency = data[offset:offset + 5]
+        result[current_currency[1]] = {
+            'quantity': int(current_currency[2]),
+            'rate': float(current_currency[4].replace(',', '.'))
+        }
+        offset += 5
+
+    return result
