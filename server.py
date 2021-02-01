7dce7537c744811a8cdaae7dac6929d01ffdfa7d
kk <kkaloshin@dengabank.ru>
2021-02-02 00:55:53 +0300

    add: Added handling errors. Added docstrings.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import json
+import typing
 from decimal import getcontext, ROUND_HALF_UP
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
@@ -6,14 +7,23 @@ from scraper import get_currency
 from settings import SERVER_ADDRESS
 
 
-class PostHandler(BaseHTTPRequestHandler):
-    """"""
+class CurrencyConverterHandler(BaseHTTPRequestHandler):
+    """Converts other currencies to RUB"""
     required_fields = {
         'currency': str,
         'quantity': int,
     }
 
-    def convert_to_rub(self, quantity: int, currency_obj: dict):
+    def _send_error(self, error: dict, status_code=400) -> None:
+        """Sends error response and corresponding headers"""
+        self.send_response(status_code)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Connection', 'close')
+        self.end_headers()
+
+        self.wfile.write(json.dumps(error).encode('utf-8'))
+
+    def convert_to_rub(self, quantity: int, currency_obj: dict) -> float:
         """
         Converts requested currency to rub
         :param quantity:
@@ -25,7 +35,7 @@ class PostHandler(BaseHTTPRequestHandler):
         return float(res)
 
     def validate(self, data: dict) -> list:
-        """"""
+        """Validates request"""
         errors = []
 
         for field, field_type in self.required_fields.items():
@@ -33,30 +43,38 @@ class PostHandler(BaseHTTPRequestHandler):
                 value = data[field]
                 assert isinstance(value, field_type)
             except KeyError:
-                errors.append(f'{field} doesnt exist')
+                errors.append(f"Field '{field}' does not exist")
             except AssertionError:
-                errors.append(f'{field} isnt type {field_type}')
+                errors.append(f"Field '{field}' is not type of {field_type}")
 
         return errors
 
-    def get_request(self) -> dict:
-        """"""
+    def get_request(self) -> typing.Optional[dict]:
+        """Gets request data and converts data to dict"""
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         try:
             request = json.loads(post_data)
         except json.JSONDecodeError:
-            raise Exception
+            return None
 
         return request
 
-    def do_POST(self):
-        """"""
+    def do_POST(self) -> None:
+        """Handles POST method"""
         request = self.get_request()
+        if not request:
+            error = {'error': 'Error was occurred during deserializing request data'}
+            self._send_error(error)
+            return
 
         errors = self.validate(request)
         if errors:
-            print(f"\nerrors: {errors}")
+            error = {
+                'error': '. '.join(errors)
+            }
+            self._send_error(error)
+            return
 
         currencies = get_currency()
         currency = currencies.get(request['currency'].upper())
@@ -73,8 +91,9 @@ class PostHandler(BaseHTTPRequestHandler):
                 'converted_value': converted_value
             }
         else:
-            status_code = 404
-            response = {'error': 'Requested currency does not supported'}
+            error = {'error': 'Requested currency does not supported'}
+            self._send_error(error)
+            return
 
         self.send_response(status_code)
         self.send_header('Content-type', 'application/json')
@@ -87,6 +106,6 @@ if __name__ == '__main__':
     getcontext().prec = 4
     getcontext().rounding = ROUND_HALF_UP
 
-    handler = PostHandler
+    handler = CurrencyConverterHandler
     httpd = HTTPServer(SERVER_ADDRESS, handler)
     httpd.serve_forever()
